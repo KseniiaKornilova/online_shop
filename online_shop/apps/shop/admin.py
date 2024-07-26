76ceed040306c2352e78d0c02c80c810fceb6923
Kseniia Kornilova <ksyu.kornilova2809@gmail.com>
2024-07-26 16:00:32 +0500

    feat: register admin classes for models

--- online_shop/apps/shop/admin.py
+++ online_shop/apps/shop/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+    search_fields = ['name',]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'available']
+    list_filter = ['category',]
+    list_editable = ['price', 'available']
